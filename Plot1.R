1db049d9e2f1bd93f647546985debccc9972a5b6
Talley Lattimore <tl3103a@student.american.edu>
2015-12-13 13:10:30 -0500

    Updated plots

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,11 @@
 # Read the file into R
 # fread is faster that data.table, which is important given the size of this file
+library(data.table)
 data <- fread("household_power_consumption.txt", sep = ";")
 
 # Convert the date to a date format and subset the dataset
 data$Date = as.Date(data$Date, format = "%d/%m/%Y")
+data1 <- data
 data1 <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 #Create a separate variable with the date and time and convert it to a useable format
